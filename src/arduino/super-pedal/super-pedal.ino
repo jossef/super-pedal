9410be8a0fe04866afb25323f912d3427398acf3
Jossef Harush <jossef12@gmail.com>
2015-10-24 19:32:50 +0300

    adding the final code

--- src/arduino/super-pedal/super-pedal.ino
+++ src/arduino/super-pedal/super-pedal.ino
@@ -1,23 +1,21 @@
-int buttonPin = 10;  // change to whatever you want
-int ledPin = 13; // just using for example
+int pedalPin = 10;
+int ledPin = 13; 
 
 void setup()
 {
-  pinMode(ledPin, OUTPUT);     // LED as output
-  pinMode(buttonPin, INPUT);    // button as input
-  digitalWrite(buttonPin, HIGH); // turns on pull-up resistor after input
+  pinMode(ledPin, OUTPUT);     
+  pinMode(pedalPin, INPUT);    
+  digitalWrite(pedalPin , HIGH);
   Serial.begin(9600);
-
 }
 
 void loop()
 {
   unsigned long startTime = millis();
-  
-  
-  while (digitalRead(buttonPin) == LOW)  // when pin goes LOW
+    
+  while (digitalRead(pedalPin) == LOW)  
   {
-    digitalWrite(ledPin, HIGH);           // turn on LED
+    digitalWrite(ledPin, HIGH);
   }
   
   unsigned long duration = millis() - startTime;
@@ -25,17 +23,29 @@ void loop()
   if (duration > 1000)
   {
     Serial.println("password");  
+    Keyboard.print("password");
   }
   else if (duration > 500)
   {
     Serial.println("copy");  
+    
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('c');
+    delay(100);
+    Keyboard.releaseAll();
+    
   }
   else if (duration > 50)
   {
     Serial.println("paste");  
+    
+    Keyboard.press(KEY_LEFT_CTRL);
+    Keyboard.press('v');
+    delay(100);
+    Keyboard.releaseAll();
+    
   }
 
-  
-  digitalWrite(ledPin, LOW);        // well, turns led off!
+  digitalWrite(ledPin, LOW); 
 
 }
